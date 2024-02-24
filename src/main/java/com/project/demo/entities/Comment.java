c92e1583a79d61581ab33f5addd048660496dfbc
Nizami Sevindi <nsevindi87@gmail.com>
2024-02-25 00:24:27 +0100

    post comment and like entities table connection ondelete

--- src/main/java/com/project/demo/entities/Comment.java
+++ src/main/java/com/project/demo/entities/Comment.java
@@ -1,7 +1,10 @@
 package com.project.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name="comment")
@@ -9,8 +12,19 @@ import lombok.Data;
 public class Comment {
     @Id
     Long id;
-    Long postId;
-    Long UserId;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable=false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
+
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable=false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
 
     @Lob
     @Column(columnDefinition="text")
