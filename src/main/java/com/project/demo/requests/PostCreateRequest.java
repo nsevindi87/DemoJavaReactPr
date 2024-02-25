187efb6ba1e65b4027d30993b30ec2793a2f62d6
Nizami Sevindi <nsevindi87@gmail.com>
2024-02-25 11:49:16 +0100

    post service

---
+++ src/main/java/com/project/demo/requests/PostCreateRequest.java
@@ -0,0 +1,11 @@
+package com.project.demo.requests;
+
+import lombok.Data;
+
+@Data
+public class PostCreateRequest {
+    Long id;
+    String text;
+    String title;
+    Long userId;
+}
--- src/main/java/com/project/demo/services/PostService.java
+++ src/main/java/com/project/demo/services/PostService.java
@@ -1,7 +1,9 @@
 package com.project.demo.services;
 
 import com.project.demo.entities.Post;
+import com.project.demo.entities.User;
 import com.project.demo.repos.PostRepository;
+import com.project.demo.requests.PostCreateRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +12,11 @@ import java.util.Optional;
 @Service
 public class PostService {
     private PostRepository postRepository;
+    private UserService userService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository,UserService userService) {
         this.postRepository = postRepository;
+        this.userService=userService;
     }
 
     public List<Post> getAllPosts(Optional<Long> userId) {
@@ -23,5 +27,17 @@ public class PostService {
 
     public Post getOnePostById(Long postId) {
         return postRepository.findById(postId).orElse(null);
+    }
+
+    public Post createOnePost(PostCreateRequest newPostRequest) {
+        User user = userService.getOneUser((newPostRequest.getUserId()));
+        if(user==null)
+            return null;
+        Post toSave = new Post();
+        toSave.setId(newPostRequest.getId());
+        toSave.setText(newPostRequest.getText());
+        toSave.setTitle(newPostRequest.getTitle());
+        toSave.setUser(user);
+        return postRepository.save(toSave);
     }
 }
