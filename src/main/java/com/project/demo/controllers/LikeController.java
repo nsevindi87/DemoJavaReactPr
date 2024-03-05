c8f980ef6a7aaf9d8e50ab465fb9e0763c92f999
Nizami Sevindi <nsevindi87@gmail.com>
2024-03-05 22:55:43 +0100

    like controller

--- src/main/java/com/project/demo/controllers/LikeController.java
+++ src/main/java/com/project/demo/controllers/LikeController.java
@@ -1,4 +1,9 @@
 package com.project.demo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/like")
 public class LikeController {
 }
